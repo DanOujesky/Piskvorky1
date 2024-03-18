d644e346431f8dca2b8f64a585e9114eef04df9f
sramek4 <sramekl4@spsejecna.cz>
2024-03-18 10:12:59 +0100

    Added new method for creating buttons

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,2 +1,34 @@
-public class Buttons {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Buttons extends JFrame implements ActionListener {
+
+    public JButton [][] buttons;
+
+    public Buttons() {
+        setTitle("Piškvorky");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 450);
+        setLayout(new GridLayout(3, 3));
+
+        buttons = new JButton[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font("Arial", Font.BOLD, 50));
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
