85b8b282a95fc02bb7b43bfbe9ed8f99e11e365d
oujesky <oujesky@spsejecna.cz>
2024-03-18 10:01:50 +0100

    Added width and height of the window

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+    private static int WIDTH = 500;
+    private static int HEIGHT = 500;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 
     public void launchGame(){}
 }
