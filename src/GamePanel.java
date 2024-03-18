b5a8c207379995504e80f22ef272cf71539e7719
sramek4 <sramekl4@spsejecna.cz>
2024-03-18 10:29:03 +0100

    Added new method actionPreformed

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
 
     private static int WIDTH = 500;
     private static int HEIGHT = 500;
+
+    private int currentPlayer;
     public JButton [][] buttons;
 
     public GamePanel(){
@@ -18,11 +21,22 @@ public class GamePanel extends JPanel {
             for (int j = 0; j < 3; j++) {
                 buttons[i][j] = new JButton("");
                 buttons[i][j].setFont(new Font("Arial", Font.BOLD, 50));
+                buttons[i][j].addActionListener(this);
                 add(buttons[i][j]);
             }
         }
 
     }
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
 
+        if (clickedButton.getText().equals("")) {
+            if (currentPlayer == 1) {
+                clickedButton.setText("X");
+            } else {
+                clickedButton.setText("O");
+            }
+        }
+    }
     public void launchGame(){}
 }
