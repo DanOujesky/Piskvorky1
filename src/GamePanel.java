007d0a3163b7c15108405d82445ff37afafb4dc3
oujesky <oujesky@spsejecna.cz>
2024-03-18 10:19:05 +0100

    added new method to add Buttons

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel {
 
     private static int WIDTH = 500;
     private static int HEIGHT = 500;
+    public JButton [][] buttons;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setLayout(new GridLayout(3, 3));
+
+        buttons = new JButton[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font("Arial", Font.BOLD, 50));
+                add(buttons[i][j]);
+            }
+        }
+
     }
 
     public void launchGame(){}
